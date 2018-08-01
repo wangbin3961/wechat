35cf575a6605e349625f1a8a009acf238cbd57b4
wangbin3961 <wangbin3961@163.com>
2018-08-01 10:25:23 +0800

    初始化

---
+++ pages/index/index.js
@@ -0,0 +1,61 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    // wx.navigateTo({
+    //   url: '../logs/logs'
+    // })
+    wx.navigateTo({
+      url: '../park/park',
+    })
+  },
+  onLoad: function () {
+    console.info('index onload')
+    if (app.globalData.userInfo) {
+      console.info(1)
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      console.info(2)
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        console.info(this.data);
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      console.info(3)
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
---
+++ pages/park/park.js
@@ -0,0 +1,99 @@
+//park.js
+//获取应用实例
+const app = getApp();
+const util = require('../../utils/util.js');
+
+Page({
+  data: {
+    latitude: 40.002607,
+    longitude: 116.487847,
+    controls: [],
+    circles: [],
+    polyline: [],
+    //标记点
+    markers: [{
+      iconPath: "img/park.png",
+      id: 0,
+      title: '11',
+      latitude: 39.97933,
+      longitude: 116.31845,
+      width: 32,
+      height: 32,
+      callout: {
+        content: '测试停车场',
+        display: 'BYCLICK',
+        color: '#345678',
+        bgColor: '#ffffff',
+        borderRadius: 3,
+        padding: 5
+      }
+    }]
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  //移动地图事件
+  regionchange: function() {
+    console.log('map region changed.');
+  },
+  //点击marker事件
+  bindMarkerTap: function(e) {
+    console.log(e);
+  },
+  bindCalloutTap: function(e) {
+    console.info(e);
+  },
+  //定位到当前位置
+  bindControlTap: function() {
+    console.log('click control.');
+    var _this = this;
+    //获取当前位置
+    wx.getLocation({
+      type: 'wgs84', //返回GPS坐标
+      success: function (res) {
+        var _latitude = res.latitude
+        var _longitude = res.longitude
+        _this.setData({
+          latitude: _latitude,
+          longitude: _longitude
+        });
+      }
+    })
+  },
+  //搜索目的地
+  bindSearchTap: function(e) {
+    console.info(e);
+    wx.navigateTo({
+      url: '../',
+    })
+  },
+  onLoad: function() {
+    console.info('park onload');
+    var _this = this;
+    //获取当前位置
+    wx.getLocation({
+      type: 'wgs84', //返回GPS坐标
+      success: function (res) {
+        console.info(res)
+        var _latitude = res.latitude
+        var _longitude = res.longitude
+        _this.setData({
+          latitude: _latitude,
+          longitude: _longitude
+        });
+      }
+    })
+    // wx.request({
+    //   url: 'http://localhost:7474',
+    //   success: res => {
+    //     console.info(res);
+    //     this.setData({
+    //       markers: res.data.parks
+    //     })
+    //   }
+    // })
+  }
+})
